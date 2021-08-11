3f83bba53855bc69e013efab6561dc17715d6652
codebender <lionelmbankeu@gmail.com>
2021-08-11 21:08:31 +0200

    updated the models for nurses i think

--- Hospital-Management-System/src/services/PatientsApi.jsx
+++ Hospital-Management-System/src/services/PatientsApi.jsx
@@ -4,35 +4,35 @@ class PatientsApi {
 
 
     static getPatients = (callback) => {
-        Axios.get('api/patients')
+        Axios.get('http://localhost:3001/api/patients')
             .then(res => callback(res.data))
             .catch(PatientsApi.errorHandler);
     }
 
 
     static getPatient = (id, callback) => {
-        Axios.get('api/patients/' + id)
+        Axios.get('http://localhost:3001/api/patients' + id)
             .then(res => callback(res.data))
             .catch(PatientsApi.errorHandler);
     }
 
 
     static admitPatient = (id, callback) => {
-        Axios.get('api/patients/' + id + '/admit')
+        Axios.get('http://localhost:3001/api/patients/' + id + '/admit')
             .then(res => callback(res.data))
             .catch(PatientsApi.errorHandler);
     }
 
 
     static dischargePatient = (id, callback) => {
-        Axios.get('api/patients/' + id + '/discharge')
+        Axios.get('http://localhost:3001/api/patients/' + id + '/discharge')
             .then(res => callback(res.data))
             .catch(PatientsApi.errorHandler);
     }
 
 
     static addPatient = (patient, callback) => {
-        Axios.post('api/patients', patient)
+        Axios.post('http://localhost:3001/api/patients', patient)
             .then(() => PatientsApi.getPatients(callback))
             .catch(PatientsApi.errorHandler);
     }
@@ -41,14 +41,14 @@ class PatientsApi {
     static editPatient = (patient, callback) => {
         let id = patient.id;
         delete patient.id;
-        Axios.put('api/patients/' + id, patient)
+        Axios.put('http://localhost:3001/api/patients' + id, patient)
             .then(() => PatientsApi.getPatients(callback))
             .catch(PatientsApi.errorHandler);
     }
 
 
     static deletePatient = (id, callback) => {
-        Axios.delete('api/patients/' + id)
+        Axios.delete('http://localhost:3001/api/patients/' + id)
             .then(() => PatientsApi.getPatients(callback))
             .catch(PatientsApi.errorHandler);
     }
